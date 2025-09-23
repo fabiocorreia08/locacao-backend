c92377564a5816efe6e3d52852d05855413a6e4c
Fabio Correia <fabiocorreia08@gmail.com>
2025-09-22 21:53:52 -0300

    primeira versao locacao

---
+++ src/test/java/br/com/locacao/LocacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.locacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
