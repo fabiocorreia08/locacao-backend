1bc1bc4327c6465611117cb00c6b8d9684631349
Fabio Correia <fabiocorreia08@gmail.com>
2025-10-15 00:03:30 -0300

    application

--- src/main/java/br/com/locacao/LocacaoApplication.java
+++ src/main/java/br/com/locacao/LocacaoApplication.java
@@ -2,12 +2,12 @@ package br.com.locacao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "br.com.locacao")
 public class LocacaoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(LocacaoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(LocacaoApplication.class, args);
+    }
 }
